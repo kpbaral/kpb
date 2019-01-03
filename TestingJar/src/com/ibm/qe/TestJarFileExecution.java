1d9301753c2ebf5e8feafe973439766b132e72fc
kpbaral <46222083+kpbaral@users.noreply.github.com>
2019-01-03 16:31:12 +0530

    Add files via upload

---
+++ TestingJar/src/com/ibm/qe/TestJarFileExecution.java
@@ -0,0 +1,19 @@
+package com.ibm.qe;
+
+import automationFramework.FirstTestCase;
+
+public class TestJarFileExecution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FirstTestCase firstTestcase = new FirstTestCase();
+		try {
+			firstTestcase.main(new String[] {""});
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
